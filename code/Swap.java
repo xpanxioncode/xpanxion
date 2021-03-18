f7e464c9aafb2c378380c31305cff53ada060ede
Raghu Khanal <rkhanal@xpanxion.com>
2021-03-18 01:17:23 -0400

    initial commit

---
+++ code/Swap.java
@@ -0,0 +1,16 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+
+        System.out.println("A is "+ a);
+        System.out.println("B is "+b);
+
+        //swaping..
+
+
+        System.out.println("A is now "+ a);
+        System.out.println("B is now "+b);
+
+    }
+}
